670ec83d26ad735b3aeb8da8e1fe8e987bfaefae
Mubashra <hafizamubashra044@gmail.com>
2024-08-25 00:25:53 +0500

    solution page

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,6 +33,33 @@ $(".review-slider").slick({
 
 
 
+$.fn.jQuerySimpleCounter = function( options ) {
+  var settings = $.extend({
+      start:  0,
+      end:    100,
+      easing: 'swing',
+      duration: 400,
+      complete: ''
+  }, options );
+
+  var thisElement = $(this);
+
+  $({count: settings.start}).animate({count: settings.end}, {
+  duration: settings.duration,
+  easing: settings.easing,
+  step: function() {
+    var mathCount = Math.ceil(this.count);
+    thisElement.text(mathCount);
+  },
+  complete: settings.complete
+});
+};
+
+
+$('#number1').jQuerySimpleCounter({end: 250,duration: 3000});
+$('#number2').jQuerySimpleCounter({end: 150,duration: 3000});
+$('#number3').jQuerySimpleCounter({end: 100,duration: 2000});
+$('#number4').jQuerySimpleCounter({end: 30,duration: 2500});
 
 
 
